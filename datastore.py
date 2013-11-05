76e51bb73196344d54fdbc49248b24761080001e
David Timms <davidtimms@btinternet.com>
2013-11-05 19:23:31 +0000

    added datastore interaction

---
+++ datastore.py
@@ -0,0 +1,69 @@
+from google.appengine.ext import ndb
+import logging
+
+class Topic(ndb.Model):
+	url = ndb.StringProperty()
+	name = ndb.StringProperty()
+	creator_id = ndb.StringProperty()
+	creator_name = ndb.StringProperty()
+
+	def to_dict(self):
+		images = map(lambda x: x.to_dict(), Image.topic_images(self.url))
+		return {
+			'topic_name': self.name,
+			'topic_url': self.url,
+			'creator_id': self.creator_id,
+			'creator_name': self.creator_name,
+			'images': images
+		}
+	@staticmethod
+	def ancestor():
+		return ndb.Key("TopicSet", "master")
+	@classmethod
+	def create(cls, topic_url, topic_name, creator_id, creator_name):
+		topic = Topic(
+			parent = cls.ancestor(),
+			url = str(topic_url),
+			name = topic_name,
+			creator_name = creator_name,
+			creator_id = creator_id)
+		return topic.put()
+
+	@classmethod
+	def retrieve(cls, url):
+		return cls.query(Topic.url == url).get()
+
+class Image(ndb.Model):
+	url = ndb.StringProperty()
+	caption = ndb.StringProperty()
+	creator_id = ndb.StringProperty()
+	creator_name = ndb.StringProperty()
+	upvoters = ndb.PickleProperty()
+	downvoters = ndb.PickleProperty()
+
+	def to_dict(self):
+		return {
+			'url': self.url,
+			'caption': self.caption,
+			'creator_id': self.creator_id,
+			'creator_name': self.creator_name,
+			'upvoters': self.upvoters,
+			'downvoters': self.downvoters
+		}
+
+	@classmethod
+	def create(cls, topic_url, image_url, image_caption, creator_id, creator_name):
+		image = Image(
+			parent = Topic.retrieve(topic_url).key,
+			url = image_url,
+			caption = image_caption,
+			creator_name = creator_name,
+			creator_id = creator_id,
+			upvoters = list(),
+			downvoters = list())
+		return image.put()
+
+	@classmethod
+	def topic_images(cls, topic_url):
+		anc_key = Topic.retrieve(topic_url).key
+		return cls.query(ancestor = anc_key).fetch(30)
--- main.py
+++ main.py
@@ -1,55 +1,88 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import jinja2
 import os
 import logging
 from google.appengine.api import users
+import datastore
 
 # Initialise jinja2 templating environment
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+def render(template_url, data):
+		template = JINJA_ENVIRONMENT.get_template(template_url)
+		return template.render(data)
 
-class TopicPageHandler(webapp2.RequestHandler):
-	def get(self, topic_url):
-		page_data = {
-			'topic_name': topic_url,
-			'topic_url': topic_url,
-			'images': ['Photo One', 'Image Two', 'Picture Three']
-		}
+class TopicHandler(webapp2.RequestHandler):
+	def topic_page(self, topic_url):
+		topic = datastore.Topic.retrieve(topic_url)
+		if topic == None:
+			self.response.write('Topic not found')
+		else:
+			page_data = topic.to_dict()
+			user = users.get_current_user()
+			if user:
+				page_data['logged_in'] = True
+				page_data['user_name'] = user.nickname()
+				page_data['user_id'] = user.user_id()
+			self.response.write(render('page.html', page_data))
 
+	def create_topic(self, topic_url):
 		user = users.get_current_user()
-		if user:
-			page_data['username'] = user.nickname()
-
-		template = JINJA_ENVIRONMENT.get_template('page.html')
-		self.response.write(template.render(page_data))
-	def post(self, topic_url):
-		res = 'Topic page created: ' + self.request.POST['topic_name']
+		topic_url = self.request.get('topic_url')
+		topic_name = self.request.get('topic_name')
+		if (not user) or (not topic_name) or (not topic_name):
+			self.response.write('Page creation failed')
+		else:
+			datastore.Topic.create(topic_url, topic_name, user.user_id(), user.nickname())
+			self.redirect(str(self.request.host_url + '/' + topic_url))
+	def delete_topic(self, topic_url):
+		res = 'Topic page deleted (not really): ' + topic_url
 		self.response.write(res)
 
-class LoginHandler(webapp2.RequestHandler):
-	def get(self):
+class ImageHandler(webapp2.RequestHandler):
+	def add_image(self, topic_url):
+		user = users.get_current_user()
+		image_url = self.request.get('image_url')
+		image_caption = self.request.get('image_caption')
+		if (not user) or (not topic_url) or (not image_url) or (not image_caption):
+			self.response.write('Image upload failed')
+		else:
+			datastore.Image.create(topic_url, image_url, image_caption, user.user_id(), user.nickname())
+			self.redirect(str(self.request.host_url + '/' + topic_url))
+
+class UserHandler(webapp2.RequestHandler):
+	def login(self):
 		return_url = self.request.host_url + '/' + self.request.GET['topic_url']
 		self.redirect(users.create_login_url(return_url))
+	def logout(self):
+		return_url = self.request.host_url + '/' + self.request.GET['topic_url']
+		self.redirect(users.create_logout_url(return_url))
 
 app = webapp2.WSGIApplication([
-	webapp2.Route('/login', handler=LoginHandler, name='login-redirect'),
-	webapp2.Route('/<topic_url>', handler=TopicPageHandler, name='topic-page')
+	webapp2.Route('/login', 
+		handler=UserHandler, 
+		name='login', 
+		handler_method='login'),
+	webapp2.Route('/logout', 
+		handler=UserHandler, 
+		name='logout', 
+		handler_method='logout'),
+	webapp2.Route('/<topic_url>/create', 
+		handler=TopicHandler, 
+		name='create-topic', 
+		handler_method='create_topic'),
+	webapp2.Route('/<topic_url>/delete', 
+		handler=TopicHandler, 
+		name='delete-topic', 
+		handler_method='delete_topic'),
+	webapp2.Route('/<topic_url>/add_image', 
+		handler=ImageHandler, 
+		name='add-image', 
+		handler_method='add_image'),
+	webapp2.Route('/<topic_url>', 
+		handler=TopicHandler, 
+		name='topic-page',
+		handler_method='topic_page')
 ], debug=True)
